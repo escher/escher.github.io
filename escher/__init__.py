3940b75c359b4b0ffcaa83564770b1ea0fe99bb2
Zachary King <zaking17@gmail.com>
2016-02-04 10:56:45 -0800

    removed everything but the hosted files

--- escher/__init__.py
+++
@@ -1,6 +0,0 @@
-from escher.version import __version__, __schema_version__, __map_model_version__
-
-from escher.plots import (Builder, get_cache_dir, clear_cache, list_cached_maps,
-                          list_cached_models, list_available_maps,
-                          list_available_models)
-
