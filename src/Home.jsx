bf67437e4311f9b192f3e8d5312ddefc8f037cf9
Zak King <zaking17@gmail.com>
2019-03-14 15:22:31 -0700

    place & style all homepage content

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,11 +3,12 @@
 import { h, Component } from 'preact'
 
 import logo2x from './img/escher-logo@2x.png'
-// import screenshot from './img/screen.png'
-// import screenshot2x from './img/screen@2x.png'
 import sbrg from './img/sbrg-logo.png'
 import nnf from './img/nnf-logo.jpg'
 import github from './img/github-icon.svg'
+import escherFbaScreen from './img/escher-fba.png'
+import structures from './img/structures.png'
+import tooltip from './img/tooltip.png'
 
 import './Home.css'
 
@@ -18,7 +19,7 @@ const TitleBox = () => (
         <img src={github} alt='GitHub' id='github-link' />
         GitHub
       </a>
-      <a href='https://github.com/zakandrewking/escher/releases' id='whats-new'>
+      <a href='https://github.com/zakandrewking/escher/releases'>
         What's new?
       </a>
     </div>
@@ -91,58 +92,77 @@ const Filters = () => (
 )
 
 const Apps = () => (
-  <div id='apps' class='row'>
-    <h2>Escher Apps</h2>
-    <div>
-      <a href='/escher-demo/knockout'>
-        <span class='demo-title'>Escher-FBA</span>
-        <img
-          alt=''
-          class='img-responsive demo-img'
-          src='/py/escher/static/img/knockout-demo-screenshot.png'
-        />
-      </a>
+  <div id='apps' class='row-collapse section'>
+    <h2 class='section-title'>Escher Apps</h2>
+    <a class='demo-box'
+      href='https://sbrg.github.io/escher-fba'
+      style={{'backgroundImage': `url(${escherFbaScreen})`}}
+    >
+      <div class='demo-title'>
+        <h3>Escher-FBA</h3>
+        Interactive, web-based flux balance analysis
+      </div>
+    </a>
+  </div>
+)
+
+const Demos = () => (
+  <div id='demos' class='row-collapse section'>
+    <div class='column section-title demo-section-title'>
+      <h2>Demos</h2>
+      <span>Technical demos for developers</span>
     </div>
-    <div>
-      <a href='/escher-demo/structures'>
-        <span class='demo-title'>Structures</span>
-        <img
-          alt=''
-          class='img-responsive demo-img'
-          src='/py/escher/static/img/structures-demo-screenshot.png'
-        />
-      </a>
-    </div>
+    <a class='demo-box'
+      href='https://escher.github.io//escher-demo/structures'
+      style={{'backgroundImage': `url(${structures}`}}
+    >
+      <div class='demo-title'>
+        <h3>Structures</h3>
+        Embed chemical structures
+      </div>
+    </a>
+    <a class='demo-box'
+      href='https://escher.github.io/escher-demo/structures'
+      style={{'backgroundImage': `url(${tooltip})`}}
+    >
+      <div class='demo-title'>
+        <h3>Tooltip</h3>
+        Customize tooltips
+      </div>
+    </a>
   </div>
 )
 
 const EscherConverter = () => (
-  <div id='escher-converter' class='row'>
-    <h2>EscherConverter</h2>
-    <p>
-      EscherConverter is a standalone program that reads files created with
-      Escher and converts them to files in community standard formats.
-    </p>
-    <p>
-      <a href='https://escher.readthedocs.org/en/stable/escherconverter.html'>
-        Learn more and download.
-      </a>
-    </p>
+  <div id='escher-converter' class='row-collapse section'>
+    <h2 class='section-title'>EscherConverter</h2>
+    <div class='column'>
+      <p>
+        EscherConverter is a standalone program that reads files created with
+        Escher and converts them to files in community standard formats.
+      </p>
+      <p>
+        <a href='https://escher.readthedocs.org/en/stable/escherconverter.html'>
+          Learn more and download.
+        </a>
+      </p>
+    </div>
   </div>
 )
 
 const FAQ = () => (
-  <div id='faq' class='row'>
+  <div class='row-collapse section'>
     <h2>FAQ</h2>
-    <ol>
+    <ol id='faq-list'>
       <li>
         <h4>What is Escher?</h4>
         <p>
           Escher is a web-based tool for building, viewing, and sharing
-          visualizations of biological pathways. These 'pathway maps' are a
+          visualizations of biological pathways. These pathway maps are a
           great way to contextualize data about metabolism. To get started,
           load a map by clicking the <b>Load Map</b> button above, or visit
-          the <a href='{{ documentation }}'>documentation</a> to learn more.
+          the <a href='https://escher.readthedocs.io'>documentation</a> to
+          learn more.
         </p>
       </li>
       <li>
@@ -161,38 +181,28 @@ const FAQ = () => (
             10.1371/journal.pcbi.1004321</a>
         </p>
       </li>
-      <li><h4>Are there more maps available? Can I contribute maps?</h4>
-        <p>
-          We will be uploading maps for all of the organisms in the
-          <a href='http://bigg.ucsd.edu/'>BiGG</a> database. If you
-          would like to contribute maps, there is a
-          <a href='https://escher.readthedocs.org/en/stable/contribute_maps.html'>guide</a>
-          available in the documentation.
-        </p>
-      </li>
       <li>
         <h4>How is Escher licensed?</h4>
-        <p>Escher and EscherConverter are distributed under the
-          <a href='https://github.com/zakandrewking/escher/blob/master/LICENSE'>
-            MIT license
-          </a>.
+        <p>
+          Escher and EscherConverter are distributed under the <a href='https://github.com/zakandrewking/escher/blob/master/LICENSE'>MIT license</a>.
         </p>
       </li>
       <li>
         <h4>What browsers can I use?</h4>
         <p>We recommend using Google Chrome for optimal performance, but
-          Escher will also run in the latest versions of Firefox, Internet
-          Explorer, and Safari (including mobile Safari).</p>
+          Escher will also run in the latest versions of Firefox, MS Edge,
+          and Safari (including mobile Chrome & Safari).</p>
       </li>
       <li>
         <h4>I have more questions. Who do I ask?</h4>
         <p>
-          Visit the <a href='{{ documentation }}'>documentation</a>
-          to get started with Escher and explore the API, or ask
-          questions in the <a href='https://gitter.im/zakandrewking/escher'>Gitter chat
-          room</a>. If you find bugs or would like to contribute to
+          Visit the <a href='https://escher.readthedocs.io'>documentation</a> to
+          get started with Escher and explore the API, or ask
+          questions in
+          the <a href='https://gitter.im/zakandrewking/escher'>Gitter chat room</a>. If
+          you find bugs or would like to contribute to
           the project, feel free to submit an issue and or a pull
-          request on <a href='{{ github }}'>Github</a>.
+          request on <a href='https://github.com/zakandrewking/escher'>Github</a>.
         </p>
       </li>
     </ol>
@@ -200,18 +210,18 @@ const FAQ = () => (
 )
 
 const Funding = () => (
-  <div id='funding' class='row'>
+  <div id='funding' class='column section'>
     <p>
       Funding provided by the Novo Nordisk Foundation through the Center
       for Biosustainability at the Technical University of Denmark
       (NNF10CC1016517).
     </p>
-    <div id='logos'>
-      <a href='http://systemsbiology.ucsd.edu/'>
-        <img id='sbrg-logo' alt='' src={sbrg} />
-      </a>
+    <div id='logos' class='row'>
       <a href='http://novonordiskfonden.dk/en'>
         <img id='nnf-logo' alt='' src={nnf} />
+      </a>
+      <a href='http://systemsbiology.ucsd.edu/'>
+        <img id='sbrg-logo' alt='' src={sbrg} />
       </a>
     </div>
   </div>
@@ -219,16 +229,19 @@ const Funding = () => (
 
 const Footer = () => (
   <footer>
-    <div class='container'>
-      <p class='more'>
-        <a href='{{ documentation }}'>Documentation</a> – <a href='{{ github }}'>GitHub</a>
-      </p>
-      <p id='copyright'>
+    <div class='footer-container column'>
+      <div class='row'>
+        <span class='more'>
+          <a href='https://escher.readthedocs.io'>Documentation</a> – <a href='https://github.com/zakandrewking/escher'>GitHub</a>
+        </span>
+        <div class='fill' />
+        <span class='version'>
+          <a href='https://github.com/zakandrewking/escher/releases'>Version {this.version}</a>
+        </span>
+      </div>
+      <span>
         © 2018 The Regents of the University of California
-      </p>
-      <p class='version'>
-        <a href='{this.github_releases}'>Version {this.version}</a>
-      </p>
+      </span>
     </div>
   </footer>
 )
@@ -250,12 +263,15 @@ export default class Home extends Component {
           <hr />
           <Apps />
           <hr />
+          <Demos />
+          <hr />
           <EscherConverter />
           <hr />
           <FAQ />
+          <hr />
           <Funding />
-          <Footer />
         </div>
+        <Footer />
       </div>
     )
   }
