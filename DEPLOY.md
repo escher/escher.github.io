86437c5a8514f16031f144c5ca403adb62d3be54
jambo <mijac0624@qq.com>
2024-10-25 15:23:29 +0100

    fix: app page can't find the json data

--- DEPLOY.md
+++ DEPLOY.md
@@ -4,6 +4,7 @@ yarn unlink escher
 yarn install
 bin/build-index
 yarn build
+yarn copy
 git commit version change
 git push commit
 ```
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  root: './src',
   build: {
-    outDir: 'docs'
-  }
+    outDir: resolve(__dirname, 'build'),
+    emptyOutDir: true,
+  },
 })
